2bf266bfab2ead338bbc168c1ab70d2db4a1d1e9
Julyemerson Leonizio <jb.leonizio@gmail.com>
2021-11-19 00:43:42 -0300

    iniciado estrutura da pagina home

--- components/organisms/Releases.vue
+++ components/organisms/Releases.vue
@@ -1,5 +1,9 @@
 <template>
-  <div></div>
+  <div class="releases">
+    <h2>Lançamentos</h2>
+
+    <BookList />
+  </div>
 </template>
 
 <script lang="ts">
@@ -8,4 +12,14 @@ import Vue from 'vue'
 export default Vue.extend({})
 </script>
 
-<style scoped></style>
+<style lang="scss" scoped>
+.releases {
+  display: grid;
+  grid-gap: 1.3rem;
+  h2 {
+    font-size: 1.375rem;
+    font-weight: 500;
+    color: color(dark, darkest);
+  }
+}
+</style>
