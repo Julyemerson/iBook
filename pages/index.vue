3048e84e13263fc8ccfe33f1e1330a71abc4a1a8
Julyemerson Leonizio <jb.leonizio@gmail.com>
2021-11-22 21:40:18 -0300

    finalizado listagem de livros

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,12 @@
 
 <script lang="ts">
 import Vue from 'vue'
+import { books } from '@/store'
+
 export default Vue.extend({
   layout: 'Ibook',
+  created() {
+    console.log(books.index())
+  },
 })
 </script>
